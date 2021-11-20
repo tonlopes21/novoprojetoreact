421d09cff84e034ea6b8efbb07ed605530ce8e2e
tonlopes21 <niltonlopes21@gmail.com>
2021-11-20 19:44:29 -0300

    Adicionado mais uma constante para utilizar com o useState

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,14 +4,16 @@ function Form (){
     function cadastrarUsuario(e){
         e.preventDefault()
         console.log(name)
+        console.log(password)
         console.log(`Cadastrou o usuario`)
 
     }
     
-    const [name, setName] = useState(`Teste`)
+    const [name, setName] = useState()
     //name = nome do tributo , o que irá resgatar
     //setName = é o nome da manipulação que será atribuída ao name.
-    //useState(), dentro de "()"é o valor que se inicia quando o component for inicializado. 
+    //useState(), dentro de "()"é o valor que se inicia quando o component for inicializado.
+    const [password, setPassword] = useState() 
 
 
     return(
@@ -26,8 +28,7 @@ function Form (){
                     <input type="text" 
                     id="name" 
                     name="name"
-                    placeholder="Digite o seu nome"
-                    value={name}
+                    placeholder="Digite o seu nome"                    
                     onChange={(e) => setName(e.target.value)}
                     
                     //Ao utilizar o onChange, fará com que qualquer alteração seja inserida no state. 
@@ -39,7 +40,8 @@ function Form (){
                     <input type="password" 
                      id="password" 
                      name="password"
-                     placeholder="Digite sua senha"/>
+                     placeholder="Digite sua senha"
+                     onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />

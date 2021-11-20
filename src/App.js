d7470b062525a4aa8723a222010e6e146b936eaa
tonlopes21 <niltonlopes21@gmail.com>
2021-11-20 19:39:54 -0300

    Formulário com pré-seleção na caixinha

--- src/App.js
+++ src/App.js
@@ -16,10 +16,7 @@ function App() {
     <h1> Testando eventos</h1>
     <Evento numero="1"/>
     <Evento numero="2"/>
-    <Evento numero="3"/>
-    <Evento numero="4"/>
-    <Evento numero="5"/>
-    <Evento numero="6"/>
+    
     <Form/>
 
      
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,10 +1,19 @@
+import { useState } from 'react'
+
 function Form (){
     function cadastrarUsuario(e){
         e.preventDefault()
+        console.log(name)
         console.log(`Cadastrou o usuario`)
 
     }
     
+    const [name, setName] = useState(`Teste`)
+    //name = nome do tributo , o que irá resgatar
+    //setName = é o nome da manipulação que será atribuída ao name.
+    //useState(), dentro de "()"é o valor que se inicia quando o component for inicializado. 
+
+
     return(
 
         <div>
@@ -13,11 +22,30 @@ function Form (){
             </h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome"/>
+                    <label htmlFor="name" >Nome:</label>
+                    <input type="text" 
+                    id="name" 
+                    name="name"
+                    placeholder="Digite o seu nome"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    
+                    //Ao utilizar o onChange, fará com que qualquer alteração seja inserida no state. 
+                    />
+                    
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input type="password" 
+                     id="password" 
+                     name="password"
+                     placeholder="Digite sua senha"/>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
                 </div>
+                
+                
             </form>
 
 

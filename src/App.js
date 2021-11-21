5a75bc27f85ca4893b1ad3ef171e4d0e97c4a1de
tonlopes21 <niltonlopes21@gmail.com>
2021-11-21 19:40:11 -0300

    Iniciando o site

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import './App.css';
-import { useState } from 'react';
-import SeuNome from './Components/SeuNome';
-import Saudacao from './Components/Saudacao';
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Components/pages/Home';
+import Empresa from './Components/pages/Empresa';
+import Contato from './Components/pages/Contato';
+import Navbar from './Components/Navbar';
+import Footer from './Components/layout/Footer';
+
 
 
 
 function App() {
-
-  
-
-const [nome, setNome] = useState()
-
-  
   return (
-    <div className={"App"}>
-    <h1> State Lift</h1>
-    <SeuNome setNome={setNome}/>
-    {nome}
-    <Saudacao nome={nome}/>
-    
-    </div>
-    
-      
-         
+    <Router>
+
+      <Navbar/>
+      <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/contato" element={<Contato />} />
+      <Route path="/empresa" element={<Empresa />} />
+      </Routes>
+          
+       <Footer/>
+    </Router>
     
   )
 }
